24920e3a694ff245d8959de68c6d3e0f401e239b
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-01-18 00:00:23 +0530

    Add tests for book and label and create database

---
+++ database/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE books (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(100),
+  publish_date DATE,
+  archived BOOLEAN DEFAULT false,
+  label_id INTEGER REFERENCES labels(id)
+);
+
+CREATE TABLE labels (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(100),
+  color VARCHAR(100)
+);
+
