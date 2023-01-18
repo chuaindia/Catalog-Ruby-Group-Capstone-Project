c5a494751609c2caea5529d69b4c4334173e455f
BELINDA <kennyegun240@gmail.com>
2023-01-18 18:50:35 +0100

    Add table for game and also correct other tables

--- database/schema.sql
+++ database/schema.sql
@@ -4,29 +4,45 @@ CREATE TABLE books (
   cover_state VARCHAR(100),
   publish_date DATE,
   archived BOOLEAN DEFAULT false,
-  label_id INTEGER REFERENCES labels(id)
+  label_id INTEGER REFERENCES labels(id),
+  items ARRAY
 );
 
 CREATE TABLE labels (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title VARCHAR(100),
-  color VARCHAR(100)
+  color VARCHAR(100),
+  items ARRAY
 );
 
 CREATE TABLE author(
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   first_name VARCHAR(100),
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
   items ARRAY
+);
+
+CREATE TABLE Game(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    multiplayer VARCHAR(100),
+    last_played_at DATE,
+    publish_date DATE,
+    archived BOOLEAN,
+    author_id INT REFERENCES Author(id),
+    label_id INT REFERENCES labels(id),
+    genre_id INT REFERENCES genre(id),
+    items ARRAY
 );
 
 CREATE TABLE album (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  on_spotify BOOLEAN
-  publish_date DATE
+  on_spotify BOOLEAN,
+  publish_date DATE,
+  items ARRAY
 );
 
 CREATE TABLE genre (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name VARCHAR(150)
+  name VARCHAR(150),
+  items ARRAY
 );
